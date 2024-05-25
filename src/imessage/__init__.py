aac9db6877bff1517b15b24a3b1853bea7e38211
Silas <63477128+SilasStokes@users.noreply.github.com>
2024-05-24 18:59:41 -0700

    removed db querying in favor of shortcuts list, thanks @merval

--- src/imessage/__init__.py
+++ src/imessage/__init__.py
@@ -1,4 +1,4 @@
-from subprocess import Popen
+from subprocess import Popen, run
 import json
 import os
 
@@ -6,11 +6,7 @@ TEMP_FILE_NAME = 'temp_py_imessage_shortcuts.json'
 REPOSITORY_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMP_FILE_PATH = os.path.join(REPOSITORY_ROOT_DIR, TEMP_FILE_NAME)
 
-HOME_PATH = os.path.expanduser('~')
-SHORTCUTS_DB_PATH = os.path.join(HOME_PATH, 
-                                'Library', 'Shortcuts', "Shortcuts.sqlite")
 SHORTCUT_NAME = 'send-imessage'
-SQL_CMD = f'SELECT ZNAME FROM ZSHORTCUT WHERE ZNAME LIKE "{SHORTCUT_NAME}";'
 
 def _dump_file(recipients: list[str], message: str) -> None:
     with open(TEMP_FILE_PATH, 'w') as f:
@@ -42,26 +38,29 @@ def send_image(recipients: list[str], message: str | None, image_path: str) -> N
         image_path,
     ])
 
-def check_shortcut_exists() -> bool:
-    """ Validates that the shortcut exists by querying the shortcuts database. 
-        Wrap in a try/except in case db connection fails.
-
-    Raises:
-        e: if sqlte3 cannot make a database connection this will propogate the exception. 
-        Most likely your application needs to be given disk access
+def check_shortcut_exists(shortcut_name:str = SHORTCUT_NAME) -> bool:
+    """ Validates that the shortcut exists
+    Args:
+        shortcut_name (str) :   an optional parameter that specifies which shortcut
+                                to look for
 
     Returns:
-        bool: True if 'send-imessage' exists in shortcuts, False if not. 
+        bool: True if shortcut_name exists in shortcuts, False if not. 
     """
-    import sqlite3
     try:
-        conn = sqlite3.connect(SHORTCUTS_DB_PATH)
-        cur = conn.cursor()
-        cur.execute(SQL_CMD)
-        shortcuts = cur.fetchall()
-        return len(shortcuts) > 0
+        result = run(["shortcuts", "list"], capture_output=True, text=True)
+        
+        if result.returncode != 0:
+            print("Error running 'shortcuts list' command")
+            return False
+        
+        shortcut_list = result.stdout.splitlines()
+        
+        return shortcut_name in shortcut_list
+    
     except Exception as e:
-        raise e
+        print(f"An error occurred: {e}")
+        return False
 
 
 def send(recipients: list[str], message: str) -> None:
